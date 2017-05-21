8595dc3d7fcc63ebee1c15cb83e805c28b3963e9
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-05-21 10:25:40 +0200

    Update SQL

--- esx_slaughtererjob.sql
+++ esx_slaughtererjob.sql
@@ -7,3 +7,10 @@ INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_
 VALUES
   ('slaughterer',0,'employee','',500,'{\"helmet_1\":0,\"decals_1\":0,\"face\":6,\"helmet_2\":0,\"glasses_2\":3,\"torso_1\":97,\"glasses_1\":6,\"tshirt_2\":0,\"arms\":99,\"decals_2\":0,\"torso_2\":0,\"pants_2\":1,\"skin\":13,\"hair_1\":2,\"pants_1\":43,\"shoes\":27,\"hair_color_2\":0,\"sex\":0,\"hair_2\":0,\"glasses\":0,\"tshirt_1\":15,\"hair_color_1\":0}','{\"tshirt_1\":15,\"decals_1\":0,\"face\":6,\"helmet_2\":0,\"glasses_2\":4,\"torso_1\":119,\"glasses_1\":5,\"tshirt_2\":0,\"arms\":123,\"decals_2\":0,\"torso_2\":2,\"pants_2\":1,\"skin\":13,\"hair_1\":2,\"pants_1\":45,\"shoes\":27,\"hair_color_2\":0,\"sex\":1,\"hair_2\":0,\"glasses\":0,\"helmet_1\":0,\"hair_color_1\":0}')
 ;
+
+INSERT INTO `items` (`name`, `label`)
+VALUES
+	('alive_chicken', 'Poulet vivant'),
+	('slaughtered_chicken', 'Poulet abattu'),
+	('packaged_chicken', 'Poulet en barquette')
+;
